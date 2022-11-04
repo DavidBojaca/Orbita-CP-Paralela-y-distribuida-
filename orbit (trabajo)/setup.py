7ef85b46650f7d1c4330407642df7e0f56cb8f66
DavidBojaca <89612071+DavidBojaca@users.noreply.github.com>
2022-11-04 07:15:43 -0500

    Delete orbit (trabajo) directory

--- orbit (trabajo)/setup.py
+++
@@ -1,8 +0,0 @@
-#Fichero para la creacion del objeto compartido
-
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-exts = (cythonize("cy_orb.pyx"))
-
-setup(ext_modules = exts)
